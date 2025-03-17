1faa5db5d4eaf3bcbf67e549eb1b06750729306a
rbhatta3 <rbhatta3@charlotte.edu>
2025-03-16 23:41:59 -0400

    Update streamlit_app.py
    Added a multi-select for Sub_Category *in the selected Category

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ st.bar_chart(df, x="Category", y="Sales")
 st.dataframe(df.groupby("Category").sum())
 # Using as_index=False here preserves the Category as a column.  If we exclude that, Category would become the datafram index and we would need to use x=None to tell bar_chart to use the index
 st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
+# added a drop down for Category
 option = st.selectbox(
     "Category",
     ("Fuirniture", "Office Supplies", "Technology"),
@@ -23,6 +24,15 @@ option = st.selectbox(
 
 st.write("You selected:", option)
 
+#Added a multi-select for Sub_Category *in the selected Category 
+options = st.multiselect(
+    "What are your favorite subcategory of the selected category",
+    ["Bookcases","Chairs","Tables","Furnishings"],iesArtPaperPaperPaperEnvelopesBindersBindersAppliancesBindersArtEnvelopesArtPaperFastenersBindersBindersLabelsPaperLabelsSuppliesPaperAppliances
+    ["Labels","Storage","Art","Binders","Appliances","Paper","Envelopes","Fasteners","Supplies"],
+    ["Phones","Accessories","Machines","Copiers"]
+)
+
+st.write("You selected:", options)
 
 # Aggregating by time
 # Here we ensure Order_Date is in datetime format, then set is as an index to our dataframe
